638eb7daa87baa3c31b4ede59d56e1552ca62c02
saket verma <saketv@pop-os.localdomain>
2021-02-10 19:34:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("every thing is working");
+
+// const input = document.querySelector('html');
+// const log = document.getElementById('log');
+
+// input.addEventListener('keydown', logKey);
+
+// function logKey(e) {
+// 	console.log(e)
+//   log.textContent += `${non(e)}`;
+// }
+
+// const non = (e) => {
+// 	if(e.key == "") return "-"
+//   	return e.key
+// }
+
+// const bob = document.querySelector('#lol');
+
+// bob.addEventListener('mousemove',doSomething);
+
+// function doSomething() {
+// 	console.log("hey there");
+// 	bob.style.border="2px solid black";
+// 	if(bob.style.background === "red")
+// 		return bob.style.background="white";
+// 	bob.style.background="red";
+// }
+
+// const input = document.querySelector("input");
+
+// input.addEventListener("keydown", log);
+
+// function log(e) {
+// 	// console.log(e)
+// 	// console.log(e.srcElement.value);
+// 	console.log(e.target.value)
+// }
